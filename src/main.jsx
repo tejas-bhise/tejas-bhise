7c40c61d78f7397d94d18abf6c377fcfee3aade6
Tejas Vitthal Bhise <tejasvitthalbhise@Tejass-MacBook-Air.local>
2026-01-28 18:43:49 +0530

    Fix Analytics import for Vite

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { inject } from '@vercel/analytics'
-import { SpeedInsights } from '@vercel/speed-insights/react'
-
-inject() 
+import { inject } from '@vercel/analytics'  
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Analytics />
-    <SpeedInsights />
   </React.StrictMode>,
 )
+
+inject()  
